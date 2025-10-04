2fc66366d33a4f89eca64243d70e8b5d69388cce
Anita <anita.nowrouzi@gmail.com>
2025-10-04 21:53:07 +0330

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import CurvedTriangle from "./components/CurvedTriangle";
+import profilePic from "./assets/images/myPics.png";
+import profileVideo from "./assets/videos/PixVerse-V5-Image-Text-360P-Ke-unscreen.gif";
+
 function App() {
   return (
     <>
       <div className="bg-black w-full h-12 flex fixed top-0 left-0 z-30">
-        <div className="bg-[#A0522D] w-full h-12 mt-12 flex">
-          <div className="grid grid-cols-4 ml-32 gap-0 pt-1 font-edu ">
-            <div className="relative bg-yellow-200 w-32 h-11 rounded-t-xl flex items-center justify-center">
-              <h3 className="text-black">Home</h3>
+        <div className="bg-notebookCover w-full h-12 mt-12 flex">
+          <div className="grid grid-cols-4 ml-32 gap-0 pt-1 font-edu text-zinc-800 text-lg">
+            <div className="relative bg-green-300 w-32 h-11 rounded-t-xl flex items-center justify-center">
+              <h3 className="">Home</h3>
               <div
                 className="absolute rounded-t-xl top-0 h-full w-32 z-50 pointer-events-none 
           shadow-[5px_0_10px_-2px_rgba(0,0,0,0.5)]"
@@ -16,15 +19,15 @@ function App() {
             </div>
 
             <div className="relative bg-blue-300 w-32 h-11 rounded-t-xl flex items-center justify-center">
-              <h3 className="text-black">Academic</h3>
+              <h3 className="">Academic</h3>
               <div
                 className="absolute rounded-t-xl top-0 h-full w-32 z-50 pointer-events-none 
           shadow-[5px_0_10px_-2px_rgba(0,0,0,0.5)]"
               ></div>
             </div>
 
-            <div className="relative bg-red-400 w-32 h-11 rounded-t-xl flex items-center justify-center">
-              <h3 className="text-black">Professional</h3>
+            <div className="relative bg-rose-400 w-32 h-11 rounded-t-xl flex items-center justify-center  ">
+              <h3 className="">Professional</h3>
               <div
                 className="absolute rounded-t-xl top-0 h-full w-32 z-50 pointer-events-none 
           shadow-[5px_0_10px_-2px_rgba(0,0,0,0.5)]"
@@ -32,7 +35,7 @@ function App() {
             </div>
 
             <div className="relative bg-yellow-200 w-32 h-11 rounded-t-xl flex items-center justify-center">
-              <h3 className="text-black">Contact</h3>
+              <h3 className="">Contact</h3>
               <div
                 className="absolute rounded-t-xl top-0 h-full w-32 z-50 pointer-events-none 
           shadow-[5px_0_10px_-2px_rgba(0,0,0,0.5)]"
@@ -41,22 +44,22 @@ function App() {
           </div>
 
           <div className="absolute top-24 left-0">
-            <div className="bg-[#A0522D] h-screen w-14 -z-10"></div>
+            <div className="bg-notebookCover h-screen w-14 -z-10"></div>
           </div>
 
           <div className=" top-24 absolute left-14">
             {" "}
-            <CurvedTriangle color="#A0522D" w={20} h={20} />
+            <CurvedTriangle color="#705220" w={20} h={20} />
           </div>
           <div className=" top-24 absolute  right-14 rotate-90 origin-center z-30 ">
             {" "}
-            <CurvedTriangle color="#A0522D" w={20} h={20} />
+            <CurvedTriangle color="#705220" w={20} h={20} />
           </div>
 
           <div className="absolute top-24 right-0">
-            <div className="bg-[#A0522D] h-screen w-14 -z-10 "></div>
+            <div className="bg-notebookCover h-screen w-14 -z-10 "></div>
           </div>
-          <div className="absolute  top-12 right-0 rotate-90 origin-center  ">
+          <div className="absolute top-12 right-0 rotate-90 origin-center  ">
             {" "}
             <CurvedTriangle />
           </div>
@@ -66,8 +69,40 @@ function App() {
           </div>
         </div>
       </div>
-      {/*   
-<div className="border-2 h-24 w-24 border-black"></div> */}
+
+      <div className="grid grid-cols-[1fr_8fr_2fr_1fr] mt-8">
+        <div></div>
+        <div className="w-full h-32 text-zinc-800 justify-self-center pr-14 pl-4  text-justify">
+          <h1 className="font-fredericka text-extrabold text-6xl text-notebookCover mb-10">
+            Hello! I'm Anita,
+          </h1>
+          <section className="font-edu text-[1.375rem]">
+            <p>
+              Frontend developer, passionate about creating innovative
+              solutions, with a background in computer science and experience
+              collaborating in professional tech teams. I enjoy tackling complex
+              problems and turning ideas into reality.
+            </p>
+            <p className="py-4">
+              When I'm not coding, you can find me painting, playing badminton
+              or experimenting with new healthy recipes. I enjoy working with
+              others to turn ideas into real projects.
+            </p>
+            <p className="text-notebookCover ">Let's connect and build something amazing together!</p>
+          </section>
+        </div>
+
+        <img
+          src={profileVideo}
+          alt="Animated waving profile"
+          className=""
+          width="300"
+        />
+
+        {/* <img src={profilePic} alt="Profile" className=" w-36 h-auto max-w-full justify-self-center" /> */}
+      </div>
+      <div></div>
+      {/* <div className="border-2 h-screen w-24 border-black"></div> */}
     </>
   );
 }
